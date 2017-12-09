6269c953362d222959c8baf1d8d89862676f83c7
ellabc23 <33379153+ellabc23@users.noreply.github.com>
2017-12-09 11:39:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username=='Ellabc23'&& password=='unicorn')||(username=='TheNovelEnchantress' && password=='dragon')||(username=='Katelyn'&& password=='alicorn')||(username=='Madison'&& password=='phoenix')){
-  window.location.href='home.html';
+if ((username == 'Ellabc23' && password == 'unicorn') ||
+    (username == 'TheNovelEnchantress' && password == 'dragon') ||
+    (username == 'Katelyn' && password == 'alicorn') ||
+    (username == 'Madison' && password == 'phoenix')) {
+    
+    window.location.href = 'home.html';
 }
 }
