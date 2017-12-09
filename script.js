9fa8b65999292baee09e8d5d7fb83dbdf31a81de
ellabc23 <33379153+ellabc23@users.noreply.github.com>
2017-12-09 10:53:01 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function login() {
+var username = document.getElementById('username').value;
+var password =  document.getElementById('password').value;
+if ((username=='Ellabc23'&& password=='unicorn')||(username=='TheNovelEnchantress' && password=='dragon')||(username=='Katelyn&& password=='alicorn')||(username=='Madison'&& password=='phoenix')
+}
