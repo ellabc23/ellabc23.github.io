720644f469d7601c7d897340aaf93b7ac966f769
Ella <33379153+ellabc23@users.noreply.github.com>
2018-09-29 11:09:44 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username == 'Ellabc23' && password == 'unicorn') ||
+if ((username == 'ellabc23' && password == 'unicorn') ||
     (username == 'TheNovelEnchantress' && password == 'dragon') ||
     (username == 'Katelyn' && password == 'alicorn') ||
     (username == 'Madison' && password == 'phoenix')) {
